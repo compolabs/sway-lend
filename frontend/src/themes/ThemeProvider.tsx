b9a2e3c7a71644a9c1267d17f6abf27b50b7858c
lzhabo <lidia.zhabo99@gmail.com>
2022-12-24 01:28:50 +0000

    changed useObserver to observer component

--- frontend/src/themes/ThemeProvider.tsx
+++ frontend/src/themes/ThemeProvider.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { ThemeProvider } from "@emotion/react";
-import { useObserver } from "mobx-react-lite";
 
 import darkTheme from "@src/themes/darkTheme";
 import lightTheme from "@src/themes/lightTheme";
 import { useStores } from "@stores";
+import { Observer } from "mobx-react-lite";
 
 export enum THEME_TYPE {
   LIGHT_THEME = "lightTheme",
@@ -22,9 +22,14 @@ export const themes = {
 //todo fix
 const ThemeWrapper: React.FC<IProps> = ({ children }) => {
   const { settingsStore } = useStores();
-  const selectedTheme = useObserver(() => settingsStore.selectedTheme);
   return (
-    <ThemeProvider theme={themes[selectedTheme]}>{children}</ThemeProvider>
+    <Observer>
+      {() => (
+        <ThemeProvider theme={themes[settingsStore.selectedTheme]}>
+          {children}
+        </ThemeProvider>
+      )}
+    </Observer>
   );
 };
 
