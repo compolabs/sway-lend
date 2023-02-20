1919b3c2fbd22b3814564b0c9f7b32f590d35901
alexey <alexnagornnyy.an@gmail.com>
2023-02-20 11:55:07 +0000

    :pool: added liquidator changes

--- services/liquidator/src/main.rs
+++ services/liquidator/src/main.rs
@@ -5,7 +5,10 @@ use serenity::prelude::*;
 use std::{env, str::FromStr, thread::sleep, time::Duration};
 
 mod utils;
-use utils::{print_swaygang_sign::print_swaygang_sign, market_abi_calls};
+use utils::{
+    market_abi_calls::market_abi_calls::{absorb, is_liquidatable},
+    print_swaygang_sign::print_swaygang_sign,
+};
 
 abigen!(Contract(
     name = "MarketContract",
@@ -13,7 +16,7 @@ abigen!(Contract(
 ));
 
 const RPC: &str = "node-beta-2.fuel.network";
-const MARKET_ADDRESS: &str = ""; 
+const MARKET_ADDRESS: &str = "0x2c290844d5b996b32cdf10de4a5294868efc3608e966a809bb03b86b2fecb2c4";
 
 #[tokio::main]
 async fn main() {
@@ -27,7 +30,10 @@ async fn main() {
     let wallet = WalletUnlocked::new_from_private_key(secret.parse().unwrap(), Some(provider));
 
     let bech32_id = Bech32ContractId::from(ContractId::from_str(MARKET_ADDRESS).unwrap());
-    let market = MarketContract::new(bech32_id, wallet.clone());
+    let market = MarketContract::new(bech32_id.clone(), wallet.clone());
+    let mut users = Users::new(MarketContract::new(bech32_id, wallet.clone()));
+    users.fetch().await;
+
     //discord
     let token = env::var("DISCORD_TOKEN").expect("❌ Expected a token in the environment");
     let client = Client::builder(&token, GatewayIntents::default())
@@ -39,10 +45,11 @@ async fn main() {
 
     print_swaygang_sign("✅ SwayLend liquidator is alive");
     loop {
-        let users = fetch_users().await;
-        for user in users {
-            if market_abi_calls::is_liquidatable(&market, user).await {
-                market_abi_calls::absorb(&market, vec![user]).await.unwrap();
+        users.fetch().await;
+        let list = users.list.clone();
+        for user in list {
+            if is_liquidatable(&market, user).await {
+                absorb(&market, vec![user]).await.unwrap();
                 channel
                     .say(
                         client.cache_and_http.http.clone(),
@@ -56,10 +63,35 @@ async fn main() {
     }
 }
 
-async fn fetch_users() -> Vec<Address> {
-    //FIXME
-    vec![
-        Address::from_str("0x1fd00ead353d0489f766e37f348432b9e12a775fdb11bf574faf22f0d02cbd5c")
-            .unwrap(),
-    ]
+struct Users {
+    pub list: Vec<Address>,
+    market: MarketContract,
+    last_check_borrowers_amount: u64,
+}
+
+impl Users {
+    fn new(market: MarketContract) -> Users {
+        Users {
+            list: vec![],
+            market,
+            last_check_borrowers_amount: 0,
+        }
+    }
+    async fn fetch(&mut self) {
+        let methods = self.market.methods();
+        let amount = methods
+            .get_borrowers_amount()
+            .simulate()
+            .await
+            .unwrap()
+            .value;
+        println!("amount = {:?}", amount);
+        let mut index = self.last_check_borrowers_amount;
+        while index < amount {
+            let borrower = methods.get_borrower(index).simulate().await.unwrap().value;
+            self.list.push(borrower);
+            index += 1;
+        }
+        self.last_check_borrowers_amount = amount;
+    }
 }
